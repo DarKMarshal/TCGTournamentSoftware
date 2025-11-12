2bd21cd5856ae533738622837b713265dd7ac636
Sean Kelly <7539622+DarKMarshal@users.noreply.github.com>
2025-11-12 16:58:44 -0500

    Initial iTournament work

---
+++ src/PlayerResult.java
@@ -0,0 +1,7 @@
+public class PlayerResult {
+    private Player player;
+    private int placement;
+    private int matchPoints;
+    private double opponentWinPercentage;
+    private int championshipPointsEarned;
+}
