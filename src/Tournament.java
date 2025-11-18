76986e70653ffe90f2812e2787d186d8beb7bb4f
Sean Kelly <7539622+DarKMarshal@users.noreply.github.com>
2025-11-18 12:03:36 -0500

    Defining the tournament classes

---
+++ src/Tournament.java
@@ -0,0 +1,9 @@
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Tournament {
+    public int id;
+    public String name;
+    public LocalDateTime date;
+    public List<PlayerResult> results;
+}
